aa5271d1346867e2866685355d59b831dcd0e584
Максим Иванов <omaxphp@yandex.ru>
2026-09-07 15:49:48 +0300

    test(eslint-plugin): cover strict decorator key sorting

--- packages/eslint-plugin/tests/rules/sort-keys-in-type-decorator/spec.ts
+++ packages/eslint-plugin/tests/rules/sort-keys-in-type-decorator/spec.ts
@@ -7,6 +7,63 @@ import { invalid, valid } from './cases';
 const ruleTester = new RuleTester();
 
 ruleTester.run(RULE_NAME, rule, {
-  valid,
-  invalid,
+  valid: [
+    ...valid,
+    {
+      code: `
+        @Component({
+          selector: 'app-root',
+          custom: true
+        })
+        class Test {}
+      `,
+      options: [
+        {
+          Component: ['selector'],
+          strict: false,
+        },
+      ],
+    },
+    {
+      code: `
+        @Component({
+          selector: 'app-root',
+          template: '<div></div>'
+        })
+        class Test {}
+      `,
+      options: [
+        {
+          Component: ['selector', 'template'],
+          strict: true,
+        },
+      ],
+    },
+  ],
+  invalid: [
+    ...invalid,
+    {
+      code: `
+        @Component({
+          custom: true
+        })
+        class Test {}
+      `,
+      options: [
+        {
+          Component: ['selector'],
+          strict: true,
+        },
+      ],
+      errors: [
+        {
+          messageId: 'unconfiguredProperty',
+          data: {
+            decorator: 'Component',
+            property: 'custom',
+          },
+        },
+      ],
+    },
+  ],
 });
